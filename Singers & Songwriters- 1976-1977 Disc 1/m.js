eb1dbac0b9346b7952961bc5a6e76e16a9684594
littleflute <13699175164@sina.cn>
2017-07-08 15:59:14 -0500

    Add files via upload

---
+++ Singers & Songwriters- 1976-1977 Disc 1/m.js
@@ -0,0 +1,14 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "01 Still the One.mp3";		
+f[2]	= "02 Hello Old Friend.mp3";		
+f[3]	= "03 Southern Nights.mp3";		
+f[4]	= "04 If You Leave Me Now.mp3";		
+f[5]	= "05 Sara Smile.mp3";		
+f[6]	= "06 Blue Bayou.mp3";		
+f[7]	= "07 You Are the Woman.mp3";		
+f[8]	= "08 Welcome Back.mp3";		
+f[9]	= "09 I'd Really Love to See You Tonight.mp3";		
+f[10]	= "10 Don't Go Breaking My Heart.mp3";		
+f[11]	= "11 Get Closer.mp3";		
+f[12]	= "12 Dream Weaver.mp3";		

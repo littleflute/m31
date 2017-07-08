a2dde7f2ec4b02e8a87d0c40468ad875202db26e
littleflute <13699175164@sina.cn>
2017-07-08 16:10:51 -0500

    Add files via upload

---
+++ Singers & Songwriters- 1976-1977 Disc 2/m.js
@@ -0,0 +1,14 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "01 Say You Love Me.mp3";		
+f[2]	= "02 Year of the Cat.mp3";		
+f[3]	= "03 On and On.mp3";		
+f[4]	= "04 (Your Love Keeps Lifting Me) Higher and Higher.mp3";		
+f[5]	= "05 Lonely Boy.mp3";		
+f[6]	= "06 We Just Disagree.mp3";		
+f[7]	= "07 Fooled Around and Fell in Love.mp3";		
+f[8]	= "08 Tracks of My Tears.mp3";		
+f[9]	= "09 Right Time of the Night.mp3";		
+f[10]	= "10 I'm in You.mp3";		
+f[11]	= "11 All By Myself.mp3";		
+f[12]	= "12 Lost Without Your Love.mp3";		
